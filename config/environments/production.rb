0f254f14f71229f62dee52ed6d65dfb1f1ec490b
Jose Veliz <jveliz3821@gmail.com>
2019-09-03 23:31:34 -0400

    add smtp settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,17 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'phootoo.herokuapp.com', :protocol => 'https' }
 
